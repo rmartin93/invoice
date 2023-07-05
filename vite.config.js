df360b44423d99bbf939fc3d4ced7041f5051764
Ryan Martin <ryan@meraki-digital.com>
2023-07-05 13:41:01 -0500

    tweak

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	// env variables
+	define: {
+		VITE_SUPABASE_ANON_KEY: process.env.VITE_SUPABASE_ANON_KEY,
+		VITE_SUPABASE_URL: process.env.VITE_SUPABASE_URL,
+	},
+});
